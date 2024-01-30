a3556f2f226fe1194d9f3bbe19d94eb71105b7a1
Srinivas Reddy <srinivasreddy@Srinivass-MacBook-Air.local>
2024-01-30 17:09:10 +0530

    vercel_v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    proxy: {
+      // Proxy all requests with path starting with '/api' to the specified target
+      '/api': {
+        target: 'https://hhbackend.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+};
